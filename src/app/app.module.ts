0d33f9026e4ed3aec797561631913375c6264fd0
Bismarck21 <darknin@gmail.com>
2018-03-16 18:26:23 -0500

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PaginaInicioComponent } from './componentes/pagina-inicio/pagina-inicio.component';
+import { PaginaRegistroComponent } from './componentes/pagina-registro/pagina-registro.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
+import { PersonalComponent } from './componentes/personal/personal.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicioComponent,
+    PaginaRegistroComponent,
+    NavegacionComponent,
+    InicioSesionComponent,
+    PersonalComponent,
+    NoEncontradoComponent
   ],
   imports: [
     BrowserModule,
