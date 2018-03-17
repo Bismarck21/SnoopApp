86b3cb6572c4058418a6da8e732e628877a0f5c6
Bismarck21 <darknin@gmail.com>
2018-03-16 20:29:43 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioSesionComponent } from './componentes/inicio-sesion/inicio-sesion.component';
+import { PaginaRegistroComponent } from './componentes/pagina-registro/pagina-registro.component';
+import { PaginaInicioComponent } from './componentes/pagina-inicio/pagina-inicio.component';
+import { PersonalComponent } from './componentes/personal/personal.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+
+const routes: Routes = [
+  {path: '', component: PaginaInicioComponent},
+  {path: 'iniciar', component: InicioSesionComponent},
+  {path: 'registro', component: PaginaRegistroComponent},
+  {path: 'personal', component: PersonalComponent},
+  {path: '**', component: NoEncontradoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
