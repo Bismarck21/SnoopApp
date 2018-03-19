68b11097165579940e34cb1e3d6036eb91657a0b
Bismarck21 <darknin@gmail.com>
2018-03-19 18:37:22 -0500

    CambiosFirebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCLQJ9rup7C7_yA9rHlntRnWIjdEsvIYdM",
+    authDomain: "snoopapp-fe1aa.firebaseapp.com",
+    databaseURL: "https://snoopapp-fe1aa.firebaseio.com",
+    projectId: "snoopapp-fe1aa",
+    storageBucket: "",
+    messagingSenderId: "843582119231"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCLQJ9rup7C7_yA9rHlntRnWIjdEsvIYdM",
+    authDomain: "snoopapp-fe1aa.firebaseapp.com",
+    databaseURL: "https://snoopapp-fe1aa.firebaseio.com",
+    projectId: "snoopapp-fe1aa",
+    storageBucket: "",
+    messagingSenderId: "843582119231"
+  }
 };
