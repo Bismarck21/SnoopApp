99b59a57d0d3ae20b934d436c50599c30faf4972
Bismarck21 <darknin@gmail.com>
2018-04-04 13:36:52 -0500

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { PaginaRegistroComponent } from './componentes/pagina-registro/pagina-re
 import { PaginaInicioComponent } from './componentes/pagina-inicio/pagina-inicio.component';
 import { PersonalComponent } from './componentes/personal/personal.component';
 import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: PaginaInicioComponent},
   {path: 'iniciar', component: InicioSesionComponent},
   {path: 'registro', component: PaginaRegistroComponent},
-  {path: 'personal', component: PersonalComponent},
+  {path: 'personal', component: PersonalComponent, canActivate: [AuthGuard]},
   {path: '**', component: NoEncontradoComponent}
 ];
 
