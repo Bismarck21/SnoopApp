31bedfefa75cdd621ea657f2d9dd4c982efd6d34
Bismarck21 <darknin@gmail.com>
2018-05-21 09:18:28 -0500

    Editar Actividad

--- src/app/componentes/editar-actividad/editar-actividad.component.ts
+++ src/app/componentes/editar-actividad/editar-actividad.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ActividadInterface } from '../../modelos/actividad';
+import { ActividadService } from '../../servicios/actividad.service';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-editar-actividad',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-actividad.component.scss']
 })
 export class EditarActividadComponent implements OnInit {
+  idActividad: string;
+  actividad: ActividadInterface = {
+    id: '',
+    titulo: '',
+    descripcion: '',
+    fecha: '',
+    duracion: '',
+    participantes: '',
+    evidencia: '',
+    fechaPublicacion: '',
+    userId: '',
+    userNombre: ''
+  }
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private actividadService: ActividadService
+  ) { }
 
   ngOnInit() {
+    this.getDetallesActividad();
+  }
+
+  getDetallesActividad() {
+    this.idActividad = this.route.snapshot.params['id'];
+    this.actividadService.getOneActividad(this.idActividad).subscribe(actividad => this.actividad = actividad);
+  }
+
+  onModificarActividad({ value }: { value: ActividadInterface }) {
+    value.id = this.idActividad;
+    this.actividadService.updateActividad(value);
+    this.router.navigate(['/ver-actividad/' + this.idActividad]);
   }
 
 }
