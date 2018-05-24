16c8b9b2a6f24b3c522eb096f7fd7dc47c478870
Bismarck21 <darknin@gmail.com>
2018-05-24 16:42:13 -0500

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 
 export const environment = {
   production: false,
-  firebaseConfig:{
+  firebaseConfig: {
     apiKey: "AIzaSyCLQJ9rup7C7_yA9rHlntRnWIjdEsvIYdM",
     authDomain: "snoopapp-fe1aa.firebaseapp.com",
     databaseURL: "https://snoopapp-fe1aa.firebaseio.com",
     projectId: "snoopapp-fe1aa",
-    storageBucket: "",
+    storageBucket: "snoopapp-fe1aa.appspot.com",
     messagingSenderId: "843582119231"
   }
 };
