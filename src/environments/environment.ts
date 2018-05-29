035c8899b4b0200b8bf375b81db3194cfd55bc18
Bismarck21 <darknin@gmail.com>
2018-05-29 11:30:23 -0500

    Ahora si final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCLQJ9rup7C7_yA9rHlntRnWIjdEsvIYdM",
-    authDomain: "snoopapp-fe1aa.firebaseapp.com",
-    databaseURL: "https://snoopapp-fe1aa.firebaseio.com",
-    projectId: "snoopapp-fe1aa",
-    storageBucket: "snoopapp-fe1aa.appspot.com",
-    messagingSenderId: "843582119231"
+    apiKey: "AIzaSyAsJVw_iveVxwypkFzOjAXhGiLTzLvkkGY",
+    authDomain: "trabajosocialup-f19aa.firebaseapp.com",
+    databaseURL: "https://trabajosocialup-f19aa.firebaseio.com",
+    projectId: "trabajosocialup-f19aa",
+    storageBucket: "trabajosocialup-f19aa.appspot.com",
+    messagingSenderId: "196395774420"
   }
 };
