cd84fbf31ffac628f7ba934df72c390686ca3f9f
Bismarck21 <darknin@gmail.com>
2018-05-30 14:25:15 -0500

    Arreglos home

--- src/app/servicios/actividad.service.ts
+++ src/app/servicios/actividad.service.ts
@@ -13,7 +13,7 @@ export class ActividadService {
   actividad: Observable<ActividadInterface>;
   constructor(
     private afs: AngularFirestore) {
-    this.actividadCollection = this.afs.collection('actividades', ref => ref.limit(14));
+    this.actividadCollection = this.afs.collection('actividades', ref => ref.limit(20));
   }
 
   addNewActividad(actividad: ActividadInterface) {
