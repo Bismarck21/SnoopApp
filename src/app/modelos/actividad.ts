8e1786a7ff8facdaf264ecba737870ba1fb1d6b7
Bismarck21 <darknin@gmail.com>
2018-06-09 16:39:34 -0500

    Cambios

--- src/app/modelos/actividad.ts
+++ src/app/modelos/actividad.ts
@@ -8,10 +8,15 @@ export interface ActividadInterface {
     duracion?: any;
     participantes?: any;
     evidencia?: any;
+    evinombre?: any;
     evidencia2?: any;
+    evinombre2?: any;
     evidencia3?: any;
+    evinombre3?: any;
     evidencia4?: any;
+    evinombre4?: any;
     evidencia5?: any;
+    evinombre5?: any;
     tarea?: any;
     grupo?: any;
     fechaPublicacion?: any;
